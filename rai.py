345d638fe560614e1da9e74c358f6c1cfb267d32
Gabriele <gabboxl0@gmail.com>
2019-02-15 16:44:37 +0100

    aggiunti nomi canali al dictionario

--- rai.py
+++ rai.py
@@ -1,17 +1,22 @@
 def mooseca(argument):
+    stream = 'https://mediapolis.rai.it/relinker/relinkerServlet.htm?cont='
     switcher = {
-        1: ["Rai 1", "test"],
+        1: ["Rai 1", "2606803"],
         2: ["Rai 2", "test2"],
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December"
+        3: ["Rai 3", "test2"],
+        4: ["Rai 4", "test2"],
+        5: ["Rai 5", "test2"],
+        6: ["Rai News24", "test2"],
+        7: ["Rai Sport + HD", "test2"],
+        8: ["Rai Sport", "test2"],
+        9: ["Rai Movie", "test2"],
+        10: ["Rai Premium", "test2"],
+        11: ["Rai Yoyo", "test2"],
+        12: ["Rai Gulp", "test2"],
+        13: ["Rai Storia", "test2"],
+        14: ["Rai Scuola", "test2"],
+        15: ["Rai Play Sport", "test2"],
+        16: ["Rai Play Sport 1", "test2"],
     }
     for asd in switcher:
         print(str(asd)+") "+switcher[asd][0])
