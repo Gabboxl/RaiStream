4d3bc0c446a7eaca15bc8de7baecd421d7fffb0e
Gabriele <gabboxl0@gmail.com>
2019-02-16 19:05:03 +0100

    added altri codici stream + fixs

--- rai.py
+++ rai.py
@@ -5,31 +5,31 @@ def mooseca(argument):
     stream = 'https://mediapolis.rai.it/relinker/relinkerServlet.htm?cont='
     switcher = {
         1: ["Rai 1", "2606803"],
-        2: ["Rai 2", "test2"],
-        3: ["Rai 3", "test2"],
-        4: ["Rai 4", "test2"],
-        5: ["Rai 5", "test2"],
-        6: ["Rai News24", "test2"],
-        7: ["Rai Sport + HD", "test2"],
-        8: ["Rai Sport", "test2"],
-        9: ["Rai Movie", "test2"],
-        10: ["Rai Premium", "test2"],
-        11: ["Rai Yoyo", "test2"],
-        12: ["Rai Gulp", "test2"],
-        13: ["Rai Storia", "test2"],
-        14: ["Rai Scuola", "test2"],
-        15: ["Rai Play Sport", "test2"],
-        16: ["Rai Play Sport 1", "test2"],
+        2: ["Rai 2", "180116"],
+        3: ["Rai 3", "180117"],
+        4: ["Rai 4", "746966"],
+        5: ["Rai 5", "72382"],
+        6: ["Rai News24", "1"],
+        7: ["Rai Sport + HD", "4145"],
+        8: ["Rai Sport", "179975"],
+        9: ["Rai Movie", "72381"],
+        10: ["Rai Premium", "72383"],
+        11: ["Rai Yoyo", "72384"],
+        12: ["Rai Gulp", "4119"],
+        13: ["Rai Storia", "24269"],
+        14: ["Rai Scuola", "24268"],
+        15: ["Rai Play Sport", "9681540"],
+        16: ["Rai Play Sport 1", "9681597"],
         0: ["Quit"]
     }
     while(True):
         try:
             for asd in switcher:
                 print(str(asd)+") "+switcher[asd][0])
-            nums = input("Scegli un canale: ")
+            nums = int(input("Scegli un canale: "))
             if(nums == 0):
                 sys.exit(0)
-            os.system("ffplay "+stream+switcher[int(nums)][1])
+            os.system("ffplay "+stream+switcher[nums][1])
         except KeyboardInterrupt:
             sys.exit(0)
             #input("\nWuoi ancora wardare films? ")
